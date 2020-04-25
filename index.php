dd0b3f072968c8d3511d6536c15b4a3728da4799
rafiody16 <53549077+rafiody16@users.noreply.github.com>
2020-04-25 09:03:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+  <title>Kesehatanmu lo</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="SolMusic HTML Template">
+  <meta name="keywords" content="music, html">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+ 
+  <link rel="stylesheet" href="css/bootstrap.min.css"/>
+  <link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css"/>
+  <link rel="stylesheet" href="css/owl.carousel.min.css"/>
+  <link rel="stylesheet" href="css/slicknav.min.css"/>
+
+  <link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+
+  <header class="header-section clearfix">
+    <a href="index.html" class="site-logo"><img src="img/beb.png">
+    </a>
+    <div class="header-right">
+      <div class="user-panel">
+        <a href="" class="login">Logout</a>
+      </div> 
+    </div>
+    <ul class="main-menu">
+      <li></li>
+      <li></li>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="http://localhost/kesehatan/cek/pertama.php">Self Checkup</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="http://localhost/kesehatan/dashboard/index.php">Dashboard</a></li>
+    </ul>
+  </header>
+  <!-- Header section end -->
+
+  <!-- Hero section -->
+  <section class="hero-section">
+    <div class="hero-slider owl-carousel">
+      <div class="hs-item">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="hs-text">
+                <h2><span>LAWAN</span>CORONA.</h2>
+                <p>Lawan corona dengan dirumah aja. Jika anda merasa tidak enak badan 
+                segerahlah periksa atau self checkup di website ini.</p>
+                <a href="http://localhost/kesehatan/cek/pertama.php" class="site-btn">Self Checkup</a>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="hr-img">
+                <img src="img/hazmat.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="intro-section spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-title">
+            <h2>APA ITU CORONA?</h2>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <p>Penyakit coronavirus (COVID-19) adalah penyakit menular yang disebabkan oleh virus jenis baru yang belum perhan teridentifikasi pada manusia. Virus ini menyebabkan penyakit saluran pernapasan (seperti flu) dengan gejala seperti batuk, demam, dan pada kasus yang lebih serius, pneumonia. Anda dapat mencegahnya dengan mencuci tangan secara rutin dan menghindari menyentuh wajah.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="subscription-section spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="sub-text">
+            <h2>Gejala Corona</h2>
+            <h3>#stayhealthy</h3>
+            <p>Segera lakukan isolasi mandiri bila Anda mengalami gejala infeksi virus Corona (COVID-19) seperti yang telah disebutkan di atas, terutama jika dalam 2 minggu terakhir Anda berada di daerah yang memiliki kasus COVID-19 atau kontak dengan penderita COVID-19. Setelah itu, hubungi hotline COVID-19 di 119 Ext. 9 untuk mendapatkan pengarahan lebih lanjut.Bila Anda mungkin terpapar virus Corona namun tidak mengalami gejala apa pun, Anda tidak perlu memeriksakan diri ke rumah sakit, cukup tinggal di rumah selama 14 hari dan membatasi kontak dengan orang lain.</p>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <ul class="sub-list">
+            <li><img src="img/icons/check-icon.png" alt="">Pilek</li>
+            <li><img src="img/icons/check-icon.png" alt="">Sesak nafas</li>
+            <li><img src="img/icons/check-icon.png" alt="">Batuk</li>
+            <li><img src="img/icons/check-icon.png" alt="">Sakit tenggorokan</li>
+            <li><img src="img/icons/check-icon.png" alt="">Demam</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+ 
+  <section class="premium-section spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-title">
+            <h2>Mencegah Corona</h2>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <p>Berikut ini adalah beberapa cara untuk mencegah covid 19/corona 
+          yang dapat kalian lakukan dengan mudah</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6">
+          <div class="premium-item">
+            <img src="img/wesoh-8.png" alt="">
+            <h4>Mencuci Tangan </h4>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="premium-item">
+            <img src="img/jarak-8.png" alt="">
+            <h4>Physical Distancing</h4>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="premium-item">
+            <img src="img/masker-8.png" alt="">
+            <h4>Menggunakan Masker saat Keluar Rumah</h4>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="premium-item">
+            <img src="img/mblayu-8.png" alt="">
+            <h4>Berolahraga</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <footer class="footer-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-6 col-lg-7 order-lg-2">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="footer-widget">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-6 col-lg-5 order-lg-1">
+          <img src="img/beb.png" alt="">
+          <div class="copyright">Copyright </div>
+          <div class="social-links">
+            <a href=""><i class="fa fa-instagram"></i></a>
+            <a href=""><i class="fa fa-pinterest"></i></a>
+            <a href=""><i class="fa fa-facebook"></i></a>
+            <a href=""><i class="fa fa-twitter"></i></a>
+            <a href=""><i class="fa fa-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.slicknav.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/mixitup.min.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>
